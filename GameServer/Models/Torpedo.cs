9d0ec7350330da80a130a15a6ba96b45d97f91a5
Mark <iwanow32855@gmail.com>
2026-06-07 23:30:50 +0600

    lab 3: torpedo fix

--- GameServer/Models/Torpedo.cs
+++ GameServer/Models/Torpedo.cs
@@ -2,11 +2,12 @@ using GameServer.Interfaces;
 
 namespace GameServer.Models;
 
-public class Torpedo : IMovingObject
+public class Torpedo : IMovingObject, IGameObject
 {
     private Vector _position;
 
     public string Id { get; }
+    public IDictionary<string, object> Properties { get; }
     public Vector Position => _position;
     public Vector Velocity { get; }
 
@@ -15,10 +16,30 @@ public class Torpedo : IMovingObject
         Id = id;
         _position = position;
         Velocity = velocity;
+        Properties = new Dictionary<string, object>();
     }
 
     public void Move(Vector newPosition)
     {
         _position = newPosition;
+    }
+
+    public T GetProperty<T>(string key)
+    {
+        if (!Properties.ContainsKey(key))
+        {
+            throw new KeyNotFoundException($"Property '{key}' not found on object '{Id}'");
+        }
+        return (T)Properties[key];
+    }
+
+    public void SetProperty(string key, object value)
+    {
+        Properties[key] = value;
+    }
+
+    public bool HasProperty(string key)
+    {
+        return Properties.ContainsKey(key);
     }
 }
